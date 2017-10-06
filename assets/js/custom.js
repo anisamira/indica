e690788bfff6e402e5a7873dd61df688a0442f57
anisamira <anisamira@siswa.um.edu.my>
2017-10-06 11:04:13 +0800

    Asset

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+/* Write here your custom javascript codes */
+			$("#my_button").click(function(){            
+    $("#my_table").append('<tr style="height:300px"><td>Some New text</td></tr>');
+});
+
+
+
+
