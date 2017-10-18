6396e7f981a9c5c71a68fca0ea8a42bcc27f66ff
DESKTOP-6TCVI20\snzon <shahrulnizam.ismai@gmail.com>
2017-10-18 17:35:59 +0800

    Target n Achievement add on

---
+++ Test Integrate/work.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
+<?php
+	include('style_dc.php');
+	include('sidebar.php');
+	
+	$module_id		=$_SESSION['module_id'];
+	$user_id		=$_SESSION['user_id'];
+	$sql			="SELECT * FROM session where session_status='1'";
+					$result = mysql_query($sql) or die(mysql_error()); 
+					if(mysql_num_rows($result)>0)
+					{
+						while($row=mysql_fetch_array($result))
+						{
+							$_SESSION['session_name']	=$row['session_name'];
+						}
+						$session_name	=$_SESSION['session_name'];
+					}
+					else
+					{
+						echo "no data found";
+					}
+	
+	$sql			= "SELECT * FROM form WHERE session_name='$session_name' AND module_id='$module_id'";
+					$result = mysql_query($sql) or die(mysql_error()); 
+					if(mysql_num_rows($result)>0)
+					{
+						while($row=mysql_fetch_array($result))
+						{
+							$_SESSION['form_status']	=$row['form_status'];
+							$_SESSION['form_id']		=$row['form_id'];
+						}
+						$form_status	=$_SESSION['form_status'];
+						$form_id		=$_SESSION['form_id'];
+					}
+					else
+					{
+						echo "no data found";
+					}
+	
+
+	
+	?>
+
+<div class="wrapper">
+
+
+		<div class="container content-sm">		
+		<!-- !PAGE CONTENT! -->
+			<div class="w3-main" style="margin-left:300px;margin-top:43px;">	
+
+
+<div class="topnav">
+  <a class="active" href="work.php">Information</a>
+  <a href="achieve.php">Target</a>
+  <a href="doc.php">Deliverables</a>
+  <a href="issue.php">Issue</a>
+  <a href="financial.php">Financial</a>
+
+</div>
+
+<div style="padding-left:16px">
+  &nbsp&nbspWELCOME <?=$module_id;?>
+  
+
+<body>
+
+
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+</script>
+
+</body>
+ 
+  
+ <?php
+ 
+ ?>
+</div>
+
+</body>
+
+
+</div>
+    </div>
+</div>
+
+<style>
+
+
+body,html{
+    height: 100%;
+    margin: 0;
+    font-family: helvetica;
+    font-weight: 100;
+	
+}
+.container {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    -webkit-transition:  left 0.4s ease-in-out;
+    -moz-transition:  left 0.4s ease-in-out;
+    -ms-transition:  left 0.4s ease-in-out;
+    -o-transition:  left 0.4s ease-in-out;
+    transition:  left 0.4s ease-in-out;
+}
+.container.open-sidebar {
+    left: 240px;
+}
+ 
+.swipe-area {
+    position: absolute;
+    width: 50px;
+    left: 0;
+top: 0;
+    height: 100%;
+    background: #f3f3f3;
+    z-index: 0;
+}
+#sidebar {
+    background: #DF314D;
+    position: absolute;
+    width: 240px;
+    height: 100%;
+    left: -240px;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+}
+#sidebar ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+#sidebar ul li {
+    margin: 0;
+}
+#sidebar ul li a {
+    padding: 15px 20px;
+    font-size: 16px;
+    font-weight: 100;
+    color: white;
+    text-decoration: none;
+    display: block;
+    border-bottom: 1px solid #C9223D;
+    -webkit-transition:  background 0.3s ease-in-out;
+    -moz-transition:  background 0.3s ease-in-out;
+    -ms-transition:  background 0.3s ease-in-out;
+    -o-transition:  background 0.3s ease-in-out;
+    transition:  background 0.3s ease-in-out;
+}
+#sidebar ul li:hover a {
+    background: #C9223D;
+}
+.main-content {
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    position: relative;
+}
+.main-content .content{
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+padding-left: 60px;
+width: 100%;
+
+}
+.main-content .content h1{
+    font-weight: 100;
+}
+.main-content .content p{
+    width: 100%;
+    line-height: 160%;
+}
+.main-content #sidebar-toggle {
+    background: #DF314D;
+    border-radius: 3px;
+    display: block;
+    position: relative;
+    padding: 10px 7px;
+    float: left;
+}
+.main-content #sidebar-toggle .bar{
+     display: block;
+    width: 18px;
+    margin-bottom: 3px;
+    height: 2px;
+    background-color: #fff;
+    border-radius: 1px;   
+}
+.main-content #sidebar-toggle .bar:last-child{
+     margin-bottom: 0;   
+}
+</style>
+
+<style>
+body {margin:0;}
+
+.topnav {
+  overflow: hidden;
+  background-color: #332;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+</style>
+<script>
+$(document).ready(function() {
+  $("[data-toggle]").click(function() {
+    var toggle_el = $(this).data("toggle");
+    $(toggle_el).toggleClass("open-sidebar");
+  });
+     
+});
+ 
+
+</script>
+</html>
+
+
