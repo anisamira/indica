99ee96f5df589b4a9f4f9077f55347a67e6347c9
DESKTOP-6TCVI20\snzon <shahrulnizam.ismai@gmail.com>
2017-10-18 23:11:55 +0800

    add virus

--- Test Integrate/work.php
+++ Test Integrate/work.php
@@ -2,10 +2,6 @@
 <html>
 <head>
 
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</head>
-
 <?php
 	include('style_dc.php');
 	include('sidebar.php');
@@ -67,11 +63,97 @@
 
 <div style="padding-left:16px">
   &nbsp&nbspWELCOME <?=$module_id;?>
+
+<div class="table-responsive">  
+								   <table class="table table-bordered"> 
+										<tr>
+											<th></th>
+											<th colspan="5"> </br> </th>						
+											<th colspan="2">BASELINE</th>
+											<th colspan="5">TARGET</th>
+											<th colspan="4">REFERENCE</th>
+										</tr>
+										<tr> 
+											<th>No.</th>  
+											<th>Goals</th>  
+											<th>Strategies</th>
+											<th>Action Plan</th>  
+											<th>KPI</th>
+											<th>Operation Definition</th>
+											<th>Achievement 2014</th>  
+											<th>Achievement 2015</th>
+											<th>Target 2016</th>  
+											<th>Target 2017</th>  
+											<th>Target 2018</th>  
+											<th>Target 2019</th>  
+											<th>Target 2020</th>
+											<th>Ownership</th> 
+											<th>Data Source</th> 
+											<th>Estimated Cost (RM)</th> 
+											<th>Expected Financial Returns</th> 											
+										</tr>
+										<?php
+										$x=1;
+										$sql="SELECT goal.*,strategy.*, actionplan.*, kpi.*, baseline.*, target.*, reference.* , form.*
+											FROM goal 
+											JOIN strategy ON strategy.goal_id=goal.goal_id 
+											JOIN actionplan ON actionplan.strategy_id=strategy.strategy_id 
+											JOIN kpi ON kpi.actionplan_id=actionplan.actionplan_id 
+											JOIN baseline ON baseline.kpi_id=kpi.kpi_id 
+											JOIN target ON target.kpi_id=kpi.kpi_id 
+											JOIN reference ON reference.kpi_id=kpi.kpi_id
+                                            JOIN form ON form.module_id=goal.module_id											
+											WHERE goal.module_id='$module_id'
+											AND goal.session_name='$session_name'
+											AND form.form_status='Approve' ";
+											$result = mysql_query($sql) or die(mysql_error()); 
+											while($row=mysql_fetch_array($result))
+											{
+												$goal_desc		=$row['goal_desc'];
+												$strategy_desc	=$row['strategy_desc'];
+												$actionplan_desc=$row['actionplan_desc'];
+												$kpi_desc		=$row['kpi_desc'];
+												$kpi_id			=$row['kpi_id'];
+												$operation_def	=$row['operation_def'];
+												$baseline1		=$row['baseline1'];
+												$baseline2		=$row['baseline2'];
+												$target1		=$row['target1'];
+												$target2		=$row['target2'];
+												$target3		=$row['target3'];
+												$target4		=$row['target4'];
+												$target5		=$row['target5'];
+												$ownership		=$row['ownership'];
+												$data_source	=$row['data_source'];
+												$estimated_cost	=$row['estimated_cost'];
+												$exp_fin_return	=$row['exp_fin_return'];								
+												?>									
+												<tr>  
+													<td><?php echo $x;?></td>
+													<td><?php echo $goal_desc;?></td>
+													<td><?php echo $strategy_desc;?></td>
+													<td><?php echo $actionplan_desc;?></td>
+													<td><?php echo $kpi_desc;?>
+														<input type="hidden" name="kpi<?php echo $x;?>" value="<?php echo $kpi_id;?>"></input>
+													</td>
+													<td><?php echo $operation_def;?></td>
+													<td><?php echo $baseline1;?></td>
+													<td><?php echo $baseline2;?></td>
+													<td><?php echo $target1;?></td>
+													<td><?php echo $target2;?></td>
+													<td><?php echo $target3;?></td>
+													<td><?php echo $target4;?></td>
+													<td><?php echo $target5;?></td>
+													<td><?php echo $ownership;?></td>
+													<td><?php echo $data_source;?></td>
+													<td><?php echo $estimated_cost;?></td>
+													<td><?php echo $exp_fin_return;?></td>
+												</tr>
+												<?php $x++;
+											} ?>
+									</table>								
+
+</div>
   
-
-<body>
-
-
 <script>
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -88,132 +170,13 @@ for (i = 0; i < acc.length; i++) {
   }
 }
 </script>
-
-</body>
  
-  
- <?php
- 
- ?>
 </div>
-
-</body>
-
 
 </div>
     </div>
 </div>
 
-<style>
-
-
-body,html{
-    height: 100%;
-    margin: 0;
-    font-family: helvetica;
-    font-weight: 100;
-	
-}
-.container {
-    position: relative;
-    height: 100%;
-    width: 100%;
-    left: 0;
-    -webkit-transition:  left 0.4s ease-in-out;
-    -moz-transition:  left 0.4s ease-in-out;
-    -ms-transition:  left 0.4s ease-in-out;
-    -o-transition:  left 0.4s ease-in-out;
-    transition:  left 0.4s ease-in-out;
-}
-.container.open-sidebar {
-    left: 240px;
-}
- 
-.swipe-area {
-    position: absolute;
-    width: 50px;
-    left: 0;
-top: 0;
-    height: 100%;
-    background: #f3f3f3;
-    z-index: 0;
-}
-#sidebar {
-    background: #DF314D;
-    position: absolute;
-    width: 240px;
-    height: 100%;
-    left: -240px;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-}
-#sidebar ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-}
-#sidebar ul li {
-    margin: 0;
-}
-#sidebar ul li a {
-    padding: 15px 20px;
-    font-size: 16px;
-    font-weight: 100;
-    color: white;
-    text-decoration: none;
-    display: block;
-    border-bottom: 1px solid #C9223D;
-    -webkit-transition:  background 0.3s ease-in-out;
-    -moz-transition:  background 0.3s ease-in-out;
-    -ms-transition:  background 0.3s ease-in-out;
-    -o-transition:  background 0.3s ease-in-out;
-    transition:  background 0.3s ease-in-out;
-}
-#sidebar ul li:hover a {
-    background: #C9223D;
-}
-.main-content {
-    width: 100%;
-    height: 100%;
-    padding: 10px;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    position: relative;
-}
-.main-content .content{
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-padding-left: 60px;
-width: 100%;
-
-}
-.main-content .content h1{
-    font-weight: 100;
-}
-.main-content .content p{
-    width: 100%;
-    line-height: 160%;
-}
-.main-content #sidebar-toggle {
-    background: #DF314D;
-    border-radius: 3px;
-    display: block;
-    position: relative;
-    padding: 10px 7px;
-    float: left;
-}
-.main-content #sidebar-toggle .bar{
-     display: block;
-    width: 18px;
-    margin-bottom: 3px;
-    height: 2px;
-    background-color: #fff;
-    border-radius: 1px;   
-}
-.main-content #sidebar-toggle .bar:last-child{
-     margin-bottom: 0;   
-}
-</style>
 
 <style>
 body {margin:0;}
