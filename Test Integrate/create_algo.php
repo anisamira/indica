d4d285e1ccce62857da7c4bd7f69c4e7515b933d
njlhamdi <najelahamdi@gmail.com>
2017-10-18 10:50:14 +0800

    a

--- Test Integrate/create_algo.php
+++ Test Integrate/create_algo.php
@@ -9,7 +9,9 @@ if(isset($_POST['create'])){
     $session_name="Sesi ".$start_year."-".$end_year;
 
     $sql1 = "INSERT INTO session (session_name, session_status) VALUES ('$session_name', TRUE)";
-    $result1=mysql_query($sql1,$conn);
+    $result1=mysql_query($sql1,$conn); 
+    $sql2 = "UPDATE session SET session_status = FALSE WHERE session_name NOT IN ($session_name)";
+    $result2=mysql_query($sql2,$conn);
 
     for($x=$start_year; $x<=$end_year; $x++ ){
         $year="year".$y;
@@ -21,6 +23,7 @@ if(isset($_POST['create'])){
 
     
     }
+
 
     
     // $sql4 = "UPDATE year SET year_status = FALSE where session_id NOT IN ($session_id) ";
