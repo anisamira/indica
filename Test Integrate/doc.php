6cb9b024004daf40f4af35935af6e9bd0e954fdf
DESKTOP-6TCVI20\snzon <shahrulnizam.ismai@gmail.com>
2017-10-23 23:50:01 +0800

    ecidence add
    -result return not empty
    -need to revise

--- Test Integrate/doc.php
+++ Test Integrate/doc.php
@@ -65,7 +65,101 @@
   &nbsp&nbspWELCOME <?=$module_id;?>
  </div> 
   <body>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="evidence" enctype="multipart/form-data">
+
+
+
+
+
+
+
+
+<?php
+$module_id=$_SESSION['module_id'];
+						$x=1;
+						$sql="SELECT goal.*,strategy.*, actionplan.*, kpi.*, baseline.*, target.*, reference.*, form.*, evidence.*
+						FROM goal 
+						JOIN strategy ON strategy.goal_id=goal.goal_id 
+						JOIN actionplan ON actionplan.strategy_id=strategy.strategy_id 
+						JOIN kpi ON kpi.actionplan_id=actionplan.actionplan_id 
+						JOIN baseline ON baseline.kpi_id=kpi.kpi_id 
+						JOIN target ON target.kpi_id=kpi.kpi_id 
+						JOIN reference ON reference.kpi_id=kpi.kpi_id 
+						JOIN form ON form.module_id=goal.module_id
+						JOIN evidence ON evidence.kpi_id=kpi.kpi_id
+                        WHERE goal.module_id='$module_id'
+						AND goal.session_name='$session_name'
+						AND form.form_status='Approve'
+						";
+						$result = mysql_query($sql) or die(mysql_error()); 
+						
+						
+						?>
+				
+<?php						
+						while($row=mysql_fetch_array($result))
+						{
+							$kpi_id			=$row['kpi_id'];
+							$goal_desc		=$row['goal_desc'];
+							$kpi_desc		=$row['kpi_desc'];
+							$files_field= $row['filename'];
+                            $files_show= "Uploads/files/$files_field";
+                            $descriptionvalue= $row['desc_file'];
+						?>
+
+						<table class="table table-bordered">
+									<col width="10%">
+									<col width="20%">
+									<col width="20%">
+									<col width="30%">
+									<col width="20%">						
+
+									<tr>
+									    <th>No.</th>
+									    <th>Goal</th>
+									    <th>KPI</th>
+										<th>Description</th>
+										<th>File</th>
+									</tr>
+						
+							<tr>  
+								<td><?php echo $x;?></td>
+								<td><?php echo $goal_desc;?></td>
+								<td><?php echo $kpi_desc;?></td>
+								<td><?php echo $descriptionvalue;?></td>
+								<td><?php echo "<a href='$files_show'>$files_field</a></div>"?></td>
+
+
+							</tr>
+							<?php
+						$x++;
+						}
+						
+					
+					
+					
+					
+				    {
+					?>
+					<form action="upload.php" method="post">
+					<input type="submit" name="Evidence" value="ADD Evidence" target="blank">
+					</form>
+					<?php
+					}	
+				    
+						?>
+					
+
+</table> 
+
+
+  
+<?php
+
+if (isset($_POST['Evidence'])){
+?>  
+  
+  
+<form action="" method="post" name="evidence" enctype="multipart/form-data">
 <table class="table table-bordered">
 									<col width="10%">
 									<col width="20%">
@@ -190,7 +284,90 @@ $result = mysql_query($sql) or die(mysql_error());
 }
 								}
 }
+
+}
 ?>
+
+
+									
+
+<?php
+
+/* 
+						$module_id=$_SESSION['module_id'];
+						$x=1;
+						$sql="SELECT goal.*,strategy.*, actionplan.*, kpi.*, baseline.*, target.*, reference.*, form.*, evidence.*
+						FROM goal 
+						JOIN strategy ON strategy.goal_id=goal.goal_id 
+						JOIN actionplan ON actionplan.strategy_id=strategy.strategy_id 
+						JOIN kpi ON kpi.actionplan_id=actionplan.actionplan_id 
+						JOIN baseline ON baseline.kpi_id=kpi.kpi_id 
+						JOIN target ON target.kpi_id=kpi.kpi_id 
+						JOIN reference ON reference.kpi_id=kpi.kpi_id 
+						JOIN form ON form.module_id=goal.module_id
+						JOIN evidence ON evidence.kpi_id=kpi.kpi_id
+                        WHERE goal.module_id='$module_id'
+						AND goal.session_name='$session_name'
+						AND form.form_status='Approve'
+						";
+						$result = mysql_query($sql) or die(mysql_error()); 
+						
+						if(!empty($result))
+						{
+						?>
+
+<table class="table table-bordered">
+									<col width="10%">
+									<col width="20%">
+									<col width="20%">
+									<col width="30%">
+									<col width="20%">						
+
+									<tr>
+									    <th>No.</th>
+									    <th>Goal</th>
+									    <th>KPI</th>
+										<th>Description</th>
+										<th>File</th>
+									</tr>
+<?php									
+						while($row=mysql_fetch_array($result))
+						{
+							$kpi_id			=$row['kpi_id'];
+							$goal_desc		=$row['goal_desc'];
+							$kpi_desc		=$row['kpi_desc'];
+							$files_field= $row['filename'];
+                            $files_show= "Uploads/files/$files_field";
+                            $descriptionvalue= $row['desc_file'];
+
+						?>
+
+							<tr>  
+								<td><?php echo $x;?></td>
+								<td><?php echo $goal_desc;?></td>
+								<td><?php echo $kpi_desc;?></td>
+								<td><?php echo $descriptionvalue;?></td>
+								<td><?php echo "<a href='$files_show'>$files_field</a></div>;"?></td>
+
+
+							</tr>
+							<?php
+						$x++;
+						}
+						}
+						else
+						?>
+					
+					<form action=""<?php echo $_SERVER['PHP_SELF']; ?>"" method="post">
+					<input type="submit" name="Evidence" value="ADD Evidence">
+					</form>
+</table> 
+
+<?php 
+
+*/
+?>
+
 
 
 	</div>
