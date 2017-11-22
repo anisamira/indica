547a118f98ff49ec0af5cf2f4f80c8bfbb306a42
DESKTOP-6TCVI20\snzon <shahrulnizam.ismai@gmail.com>
2017-11-22 16:10:32 +0800

    anis updated

--- Test Integrate/workbench_view.php
+++ Test Integrate/workbench_view.php
@@ -25,6 +25,7 @@ Module Code: <input type="text" name="code">
 
 
 <input type="submit" name="search" value="Search">
+<input type="submit" name="select" value="Select ALL">
 </form>
 
 </html>        
@@ -102,6 +103,79 @@ if(isset($_POST["search"])){
 	
 }
 }
+
+// select all
+if(isset($_POST["select"])){
+	
+	
+  $x=1;
+  $sql=("SELECT goal.*, module.* FROM goal JOIN module ON goal.module_id=module.module_id GROUP BY module.module_id");
+  	
+	$result = mysql_query($sql) or die(mysql_error());
+
+
+ if(mysql_num_rows($result)>0){
+  
+	  ?>
+	  
+	  <div class="table-responsive">  
+								   <table class="table table-bordered"> 
+
+										<tr> 
+											<th>CODE</th>  
+											<th>SESSION</th>  
+											<th>NAME</th>
+										</tr>
+	  
+	  
+<?php		
+		while($row=mysql_fetch_array($result))
+		{
+			$_GET['module_id']=$row['module_id'];
+			$_GET['session_name']=$row['session_name'];
+			$name=$row['module_name'];
+			$moduleid=$_GET['module_id'];
+			$sesi=$_GET['session_name'];
+			
+			
+			
+			
+?>	
+<tr>
+                         <form action="work_view2.php?moduleid=$moduleid&sesi=$sesi" method="get">
+                            <td><?php echo $moduleid;?></td>
+								   <input type="hidden" name="moduleid" value="<?php echo $moduleid;?>"/>   
+							<td><?php echo $sesi;?></td>
+								   <input type="hidden" name="sesi" value="<?php echo $sesi;?>"/>
+							<td><?php echo $name;?></td>
+							<td><button type="submit" class="btn btn-primary">Go</button></td>
+							<!--<td><button type="submit" name="Go" class="btn btn-primary"><a href="work_view.php?moduleid=$moduleid&sesi=$sesi">Go</a></button></td>-->
+
+							</form>
+
+</tr>								   
+												<?php
+											}
+	$x++;											
+
+											?>
+									</table>								
+
+</div>
+
+<?php		
+ }	 
+  else{
+ 	{
+		//print error message
+		echo 'No code found';
+	}
+	// once processing is complete
+	// free result set
+	
+}
+}
+
 
 
 
