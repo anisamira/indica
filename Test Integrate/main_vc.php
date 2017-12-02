8f0597dbd901a065bba5e5dd309727cda9af9db8
DESKTOP-6TCVI20\snzon <shahrulnizam.ismai@gmail.com>
2017-12-02 10:20:26 +0800

    card v2

--- Test Integrate/main_vc.php
+++ Test Integrate/main_vc.php
@@ -68,22 +68,8 @@ else
 					else
 					{
 						echo "no data found";
-					}									
- $sql			= "SELECT COUNT(goal_id) AS goalie FROM goal WHERE module_id='$module_id'";
-					$result = mysql_query($sql) or die(mysql_error()); 
-					if(mysql_num_rows($result)>0)
-					{
-						while($row=mysql_fetch_array($result))
-						{
-							$goals=$row['goalie'];
-							
-						}
-						
 					}
-					else
-					{
-						echo "no data found";
-					}									
+
 
  $sql			= "SELECT * FROM year WHERE year_name='$curyear'";
 					$result = mysql_query($sql) or die(mysql_error()); 
@@ -102,8 +88,79 @@ else
 						echo "no data found";
 					}
 						
-				
-					?>
+
+$sql			= "SELECT COUNT(goal.goal_id) AS gol, COUNT(kpi.kpi_id) AS kpi, SUM(reference.estimated_cost) AS cost, SUM(reference.exp_fin_return) AS income
+						FROM goal 
+						JOIN strategy ON strategy.goal_id=goal.goal_id 
+						JOIN actionplan ON actionplan.strategy_id=strategy.strategy_id 
+						JOIN kpi ON kpi.actionplan_id=actionplan.actionplan_id 
+						JOIN baseline ON baseline.kpi_id=kpi.kpi_id 
+						JOIN target ON target.kpi_id=kpi.kpi_id 
+						JOIN reference ON reference.kpi_id=kpi.kpi_id 
+						JOIN form ON form.module_id=goal.module_id
+						JOIN achievement ON achievement.target_id=target.target_id
+						JOIN year ON achievement.year_id=year.year_id
+						JOIN evidence ON evidence.ach_id=achievement.ach_id
+						JOIN session ON session.session_name=goal.session_name
+                        WHERE goal.module_id='$module_id'
+						AND goal.session_name='$session_name'
+						AND session.session_status='1'
+						AND year.year_id='$year_id'
+						";
+					$result = mysql_query($sql) or die(mysql_error()); 
+					if(mysql_num_rows($result)>0)
+					{
+						while($row=mysql_fetch_array($result))
+						{
+							$goal_num= $row['gol'];
+							$kpi_num= $row['kpi'];
+							$cost= $row['cost'];
+							$income= $row['income'];
+						}
+						
+					}
+					else
+					{
+						echo "no data found";
+					}
+					
+$sql			= "SELECT COUNT(issue.issue_id) AS issue
+						FROM goal 
+						JOIN strategy ON strategy.goal_id=goal.goal_id 
+						JOIN actionplan ON actionplan.strategy_id=strategy.strategy_id 
+						JOIN kpi ON kpi.actionplan_id=actionplan.actionplan_id 
+						JOIN baseline ON baseline.kpi_id=kpi.kpi_id 
+						JOIN target ON target.kpi_id=kpi.kpi_id 
+						JOIN reference ON reference.kpi_id=kpi.kpi_id 
+						JOIN form ON form.module_id=goal.module_id
+						JOIN achievement ON achievement.target_id=target.target_id
+						JOIN year ON achievement.year_id=year.year_id
+						JOIN evidence ON evidence.ach_id=achievement.ach_id
+						JOIN session ON session.session_name=goal.session_name
+						JOIN issue ON issue.ach_id=achievement.ach_id
+                        WHERE goal.module_id='$module_id'
+						AND goal.session_name='$session_name'
+						AND session.session_status='1'
+						AND year.year_id='$year_id'
+						";
+					$result = mysql_query($sql) or die(mysql_error()); 
+					if(mysql_num_rows($result)>0)
+					{
+						while($row=mysql_fetch_array($result))
+						{
+							
+							$issue_num= $row['issue'];
+							
+						}
+						
+					}
+					else
+					{
+						echo "no data found";
+					}					
+					
+?>
+	
 
 
 
@@ -124,7 +181,29 @@ else
 
     <div class=" card [ is-collapsed ] ">
       <div class="card__inner [ js-expander ]">
-        <span>Card</span>
+        <span>Goals</span>
+        <i class="fa fa-folder-o"></i>
+      </div>
+      <div class="card__expander">
+        <i class="fa fa-close [ js-collapser ]"></i>
+        Number of Goal: <?=$goal_num;?>
+      </div>
+    </div>
+
+    <div class=" card [ is-collapsed ] ">
+      <div class="card__inner [ js-expander ]">
+        <span>KPI</span>
+        <i class="fa fa-folder-o"></i>
+      </div>
+      <div class="card__expander">
+        <i class="fa fa-close [ js-collapser ]"></i>
+      Number of KPI: <?=$kpi_num;?>
+      </div>
+    </div>
+
+    <div class=" card [ is-collapsed ] ">
+      <div class="card__inner [ js-expander ]">
+        <span>Achievement</span>
         <i class="fa fa-folder-o"></i>
       </div>
       <div class="card__expander">
@@ -135,89 +214,67 @@ else
 
     <div class=" card [ is-collapsed ] ">
       <div class="card__inner [ js-expander ]">
-        <span>Card</span>
+        <span>Issue</span>
         <i class="fa fa-folder-o"></i>
       </div>
       <div class="card__expander">
         <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
+        Number of Issue: <?=$issue_num;?>
       </div>
     </div>
 
     <div class=" card [ is-collapsed ] ">
       <div class="card__inner [ js-expander ]">
-        <span>Card</span>
+        <span>Unachieve</span>
         <i class="fa fa-folder-o"></i>
       </div>
       <div class="card__expander">
         <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
+        Not achieve target
       </div>
     </div>
 
     <div class=" card [ is-collapsed ] ">
       <div class="card__inner [ js-expander ]">
-        <span>Carder</span>
+        <span>Expenditure</span>
         <i class="fa fa-folder-o"></i>
       </div>
       <div class="card__expander">
         <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
+         Total Expenditure: RM<?=$cost;?>
       </div>
     </div>
 
     <div class=" card [ is-collapsed ] ">
       <div class="card__inner [ js-expander ]">
-        <span>Card</span>
+        <span>ROI</span>
         <i class="fa fa-folder-o"></i>
       </div>
       <div class="card__expander">
         <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
+        Return on Investment: RM<?=$income;?>
       </div>
     </div>
 
     <div class=" card [ is-collapsed ] ">
       <div class="card__inner [ js-expander ]">
-        <span>Card</span>
+        <span>Health</span>
         <i class="fa fa-folder-o"></i>
       </div>
       <div class="card__expander">
         <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
+        KPI Health
       </div>
     </div>
 
     <div class=" card [ is-collapsed ] ">
       <div class="card__inner [ js-expander ]">
-        <span>Card</span>
+        <span>Status</span>
         <i class="fa fa-folder-o"></i>
       </div>
       <div class="card__expander">
         <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
-      </div>
-    </div>
-
-    <div class=" card [ is-collapsed ] ">
-      <div class="card__inner [ js-expander ]">
-        <span>Card</span>
-        <i class="fa fa-folder-o"></i>
-      </div>
-      <div class="card__expander">
-        <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
-      </div>
-    </div>
-
-    <div class=" card [ is-collapsed ] ">
-      <div class="card__inner [ js-expander ]">
-        <span>Card</span>
-        <i class="fa fa-folder-o"></i>
-      </div>
-      <div class="card__expander">
-        <i class="fa fa-close [ js-collapser ]"></i>
-        Expander
+       Current Status
       </div>
     </div>
 
@@ -227,10 +284,8 @@ else
 
 	<script type="text/javascript" src="assets/best/js/index.js"></script>
 
-  
-  
-		
-  </div>
 
+  </div>
+  
 
 
---
+++ Test Integrate/workbench_tnc.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+	include('style_dc.php');
+	include('sidebar.php');
+
+	$module_id		=$_SESSION['module_id'];
+	$user_id		=$_SESSION['user_id'];
+	$sql			="SELECT * FROM session where session_status='1'";
+					$result = mysql_query($sql) or die(mysql_error()); 
+					if(mysql_num_rows($result)>0)
+					{
+						while($row=mysql_fetch_array($result))
+						{
+							$_SESSION['session_name']	=$row['session_name'];
+						}
+						$session_name	=$_SESSION['session_name'];
+					}
+					else
+					{
+						echo "no data found";
+					}
+	
+	$sql			= "SELECT * FROM form WHERE session_name='$session_name' AND module_id='$module_id'";
+					$result = mysql_query($sql) or die(mysql_error()); 
+					if(mysql_num_rows($result)>0)
+					{
+						while($row=mysql_fetch_array($result))
+						{
+							$_SESSION['form_status']	=$row['form_status'];
+							$_SESSION['form_id']		=$row['form_id'];
+						}
+						$form_status	=$_SESSION['form_status'];
+						$form_id		=$_SESSION['form_id'];
+					}
+					else
+					{
+						echo "no data found";
+					}			
+	
+	 $sql			= "SELECT * FROM module WHERE module_id='$module_id'";
+					$result = mysql_query($sql) or die(mysql_error()); 
+					if(mysql_num_rows($result)>0)
+					{
+						while($row=mysql_fetch_array($result))
+						{
+							$module_name=$row['module_name'];
+							
+						}
+						
+					}
+					else
+					{
+						echo "no data found";
+					}	
+		
+	
+	?>
+
+<div class="wrapper">
+
+
+		<div class="container content-sm">		
+		<!-- !PAGE CONTENT! -->
+			<div class="w3-main" style="margin-left:300px;margin-top:43px;">	
+
+
+<?php
+
+// select all
+	
+  $x=1;
+  $sql=("SELECT goal.*, module.* FROM goal JOIN module ON goal.module_id=module.module_id WHERE goal.module_id='$module_id' GROUP BY module.module_id");
+  	
+	$result = mysql_query($sql) or die(mysql_error());
+
+
+ if(mysql_num_rows($result)>0){
+  
+	  ?>
+	  
+	  <div class="table-responsive">  
+								   <table class="table table-bordered"> 
+
+										<tr> 
+											<th>CODE</th>  
+											<th>SESSION</th>  
+											<th>NAME</th>
+										</tr>
+	  
+	  
+<?php		
+		while($row=mysql_fetch_array($result))
+		{
+			$_GET['module_id']=$row['module_id'];
+			$_GET['session_name']=$row['session_name'];
+			$name=$row['module_name'];
+			$moduleid=$_GET['module_id'];
+			$sesi=$_GET['session_name'];
+			
+			
+			
+			
+?>	
+<tr>
+                         <form action="work_view2.php?moduleid=$moduleid&sesi=$sesi" method="get">
+                            <td><?php echo $moduleid;?></td>
+								   <input type="hidden" name="moduleid" value="<?php echo $moduleid;?>"/>   
+							<td><?php echo $sesi;?></td>
+								   <input type="hidden" name="sesi" value="<?php echo $sesi;?>"/>
+							<td><?php echo $name;?></td>
+							<td><button type="submit" class="btn btn-primary">Go</button></td>
+							<!--<td><button type="submit" name="Go" class="btn btn-primary"><a href="work_view.php?moduleid=$moduleid&sesi=$sesi">Go</a></button></td>-->
+
+							</form>
+
+</tr>								   
+												<?php
+											}
+	$x++;											
+
+											?>
+									</table>								
+
+</div>
+
+<?php		
+ }	 
+  else{
+ 	{
+		//print error message
+		echo 'No project found';
+	}
+	// once processing is complete
+	// free result set
+	
+}
+
+
+
+
+
+?>		
+
+			</div>
+    </div>
+ 
+</div>
+  
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+</script>
+ 
+</div>
+
+</div>
+    </div>
+</div>
+
+
+<style>
+body {margin:0;}
+
+.topnav {
+  overflow: hidden;
+  background-color: #332;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+</style>
+<script>
+$(document).ready(function() {
+  $("[data-toggle]").click(function() {
+    var toggle_el = $(this).data("toggle");
+    $(toggle_el).toggleClass("open-sidebar");
+  });
+     
+});
+ 
+
+</script>
+</html>
+
+
