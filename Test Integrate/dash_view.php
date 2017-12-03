b6a5487785753011da224cb37f5f65efcee563a0
anisamira <anisamira@siswa.um.edu.my>
2017-12-03 22:44:34 +0800

    aaa

--- Test Integrate/dash_view.php
+++ Test Integrate/dash_view.php
@@ -1,21 +1,19 @@
 <?php
 	include('style_dc.php');
 	include('sidebar.php');
-	$module_id		=$_SESSION['module_id'];
-	$user_id		=$_SESSION['user_id'];
 	
 	$curyear=date ('Y');
     $date_now=date ("m/d/Y");
-    $date_q= date ("06/30/Y");
- 
-	if ($date_now<=$date_q)
+ $date_q= date ("06/30/Y");
+             if ($date_now<=$date_q)
 	{
-		$quater=1;
+			$quater=1;
 	}
-	else
-		$quater=2;	
+else
+			$quater=2;	
 	
-	$sql			="SELECT * FROM session where session_status='1'";
+  
+$sql			="SELECT * FROM session where session_status='1'";
 					$result = mysql_query($sql) or die(mysql_error()); 
 					if(mysql_num_rows($result)>0)
 					{
@@ -28,9 +26,30 @@
 					else
 					{
 						echo "no data found";
-					}
+					}					
+
+		
+		
 	
-	$sql2= "SELECT * FROM form WHERE session_name='$session_name' AND module_id='$module_id'";
+?>
+ 
+					
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/data.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+
+
+
+<div id="content">	
+	
+
+	<?php
+	 if(isset($_POST['save']))
+		{
+			
+			$module_id=$_POST['moduleid'];
+			$sesi=$_POST['sesi'];
+			$sql2= "SELECT * FROM form WHERE session_name='$sesi' AND module_id='$module_id'";
 					$result2 = mysql_query($sql2) or die(mysql_error()); 
 					if(mysql_num_rows($result2)>0)
 					{
@@ -46,84 +65,115 @@
 					{
 						echo "no data found";
 					}
-	$sql3			="SELECT module.*, user.* 
-						FROM user 
-						JOIN module 
-						ON module.module_id=user.module_id
-						WHERE user.module_id='$module_id' 
-						AND user.user_id='$user_id'";
-					$result3 = mysql_query($sql3) or die(mysql_error());  
-					while($row=mysql_fetch_array($result3))
-					{
-						
-						$_SESSION['module_name'] = $row['module_name'];
+					?>
+				<div class="control-group">
+					<label class="control-label">Year</label>
+						<div class="controls">
+								<form action="" method="post">
+									<table class="table" width="20%">
+										<tr>
+											<td>
+												<select class="form-control" name="year"><?php
 
-						}?>
+													$sql=mysql_query("Select * from session where session_name='$session_name'");
+													$row=mysql_num_rows($sql);
+													while($row = mysql_fetch_array($sql))
+													{
+														$year1=$row['year1'];
+														$year2=$row['year2'];
+														$year3=$row['year3'];
+														$year4=$row['year4'];
+														$year5=$row['year5'];
+														?>
+														
+														<option value="<?php  echo $year1;?>"><?php  echo $year1;?></option>
+														<option value="<?php  echo $year2;?>"><?php  echo $year2;?></option>
+														<option value="<?php  echo $year3;?>"><?php  echo $year3;?></option>
+														<option value="<?php  echo $year4;?>"><?php  echo $year4;?></option>
+														<option value="<?php  echo $year5;?>"><?php  echo $year5;?></option>
+									
+														<?php
 
- 
-					
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/data.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-
-
-
-<div class="w3-main" style="margin-left:300px;margin-top:60px; margin-right:50px;">	
-	<div class="control-group">
-		<label class="control-label">Year</label>
-			<div class="controls">
-					<form action="" method="post">
-						<table class="table" width="20%">
-							<tr>
-								<td>
-									<select class="form-control" name="txt_year"><?php
-
-										$sql=mysql_query("Select * from session where session_name='$session_name'");
-										$row=mysql_num_rows($sql);
-										while($row = mysql_fetch_array($sql))
-										{
-											$year1=$row['year1'];
-											$year2=$row['year2'];
-											$year3=$row['year3'];
-											$year4=$row['year4'];
-											$year5=$row['year5'];
-											?>
-											
-											<option value="<?php  echo $year1;?>"><?php  echo $year1;?></option>
-											<option value="<?php  echo $year2;?>"><?php  echo $year2;?></option>
-											<option value="<?php  echo $year3;?>"><?php  echo $year3;?></option>
-											<option value="<?php  echo $year4;?>"><?php  echo $year4;?></option>
-											<option value="<?php  echo $year5;?>"><?php  echo $year5;?></option>
-						
-											<?php
-
-										}?>
+													}?>
+													
+												</select> 
+												<input type="hidden" name="form_id" value="<?php echo $form_id;?>"></input>
+											</td>
+										<div class="form-actions">
+											<td><button type="submit" class ="btn btn-sucess"name="year" value="">Go</button></td>
+										</div>
+										</tr>
 										
-									</select> 
-								</td>
-							<div class="form-actions">
-								<td><button type="submit" class ="btn btn-sucess"name="save" value="">Go</button></td>
-							</div>
-							</tr>
+									</table>
+
+								</form>
+
 							
-						</table>
+							
+						</div>
 
-					</form>
-
-				
-				
-			</div>
-
-		</div>
-
-
-
-	
-	<?php
+					</div>
+			<?php
+			
 		
-		if(isset($_POST['save']))
+
+				
+			$sql3	="SELECT achievement.ach_id, achievement.target, achievement.ach_desc, target.target_id, kpi.kpi_id, kpi.kpi_desc, kpi.session_name, year.year_id, year.year_name
+						FROM kpi JOIN target ON target.kpi_id=kpi.kpi_id
+						JOIN achievement ON achievement.target_id=target.target_id
+						JOIN year on year.year_id=achievement.year_id
+						WHERE achievement.form_id='$form_id' 
+						AND kpi.session_name='$session_name'
+						AND achievement.quarter='$quater'
+						AND year.year_name='$curyear'";
+			$result3=mysql_query($sql3) or die (mysql_error());
+			if (mysql_num_rows($result3)>0)
+			{?>
+			<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div><div class="control-group">
+				<table class="table table-bordered" id="datatable">
+				<thead>
+					<tr>
+						<th>KPI</th>
+						<th>Target</th>
+						<th>Achievement</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php
+				while($row3=mysql_fetch_array($result3))
+				{
+					$kpi_desc	=$row3['kpi_desc'];
+					$ach_desc	=$row3['ach_desc'];
+					$ach_id		=$row3['ach_id'];
+					$target		=$row3['target'];?>
+					 <tr style="font-size:13px">
+						<td><?php echo $kpi_desc;?></td>
+						<td><?php echo $target;?></td>
+						<td><?php echo $ach_desc;?></td>
+					</tr><?php
+				}?>
+				</tbody>
+			</table>
+			</div>
+			<?php
+			}
+			else
+			{
+				?>
+							<div class="alert alert-warning alert-dismissable fade in">
+							<meta http-equiv="refresh" content="1;url=workbench_dash.php" /> 
+								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+								<strong>There is no records</strong> Redirecting in 1 seconds...
+							</div><?php	
+			}
+			
+		}
+	
+		
+		if(isset($_POST['year']))
 		{
-			$year = $_POST['txt_year'];
+			$curyear	=$_POST['year'];
+			$form_id	=$_POST['form_id'];
 			$sql2	="SELECT achievement.ach_id, achievement.target, achievement.ach_desc, target.target_id, kpi.kpi_id, kpi.kpi_desc, kpi.session_name, year.year_id, year.year_name
 						FROM kpi JOIN target ON target.kpi_id=kpi.kpi_id
 						JOIN achievement ON achievement.target_id=target.target_id
@@ -131,7 +181,7 @@
 						WHERE achievement.form_id='$form_id' 
 						AND kpi.session_name='$session_name'
 						AND achievement.quarter='2'
-						AND year.year_name='$year'";
+						AND year.year_name='$curyear'";
 			$result2=mysql_query($sql2) or die (mysql_error());
 			if (mysql_num_rows($result2)>0)
 			{?>
@@ -161,77 +211,28 @@
 				</tbody>
 			</table>
 			</div>
-			</div><?php
+			<?php
 			}
 			else
 			{
 				?>
 							<div class="alert alert-warning alert-dismissable fade in">
-								<meta http-equiv="refresh" content="1;url=graph_kpi.php" />
+							<meta http-equiv="refresh" content="1;url=workbench_dash.php" />
 								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 								<strong>There is no records</strong> Redirecting in 1 seconds...
 							</div><?php	
 			}
 			
 		}
-		else
-		{
-			$year=$curyear;
-			
-			$sql2	="SELECT achievement.ach_id, achievement.target, achievement.ach_desc, target.target_id, kpi.kpi_id, kpi.kpi_desc, kpi.session_name, year.year_id, year.year_name
-						FROM kpi JOIN target ON target.kpi_id=kpi.kpi_id
-						JOIN achievement ON achievement.target_id=target.target_id
-						JOIN year on year.year_id=achievement.year_id
-						WHERE achievement.form_id='$form_id' 
-						AND kpi.session_name='$session_name'
-						AND achievement.quarter='$quater'
-						AND year.year_name='$curyear'";
-			$result2=mysql_query($sql2) or die (mysql_error());
-			if (mysql_num_rows($result2)>0)
-			{?>
-			<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-		<div class="control-group">
-				<table class="table table-bordered" id="datatable">
-				<thead>
-					<tr>
-						<th>KPI</th>
-						<th>Target</th>
-						<th>Achievement</th>
-					</tr>
-				</thead>
-				<tbody><?php
-				while($row=mysql_fetch_array($result2))
-				{
-					$kpi_desc	=$row['kpi_desc'];
-					$ach_desc	=$row['ach_desc'];
-					$ach_id		=$row['ach_id'];
-					$target		=$row['target'];?>
-					 <tr style="font-size:13px">
-						<td><?php echo $kpi_desc;?></td>
-						<td><?php echo $target;?></td>
-						<td><?php echo $ach_desc;?></td>
-					</tr><?php
-				}?>
-				</tbody>
-			</table>
-			</div>
-			</div><?php
-			}
-			else
-			{
-				?>
-							<div class="alert alert-warning alert-dismissable fade in">
-								<meta http-equiv="refresh" content="1;url=graph_kpi.php" />
-								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-								<strong>There is no records</strong> Redirecting in 1 seconds...
-							</div><?php	
-			}
-		}?>
+		
+		?>
 		
 		   
 
 </div>
-</div>
+
+
+
 <script>
 Highcharts.chart('container', {
     data: {
@@ -241,7 +242,7 @@ Highcharts.chart('container', {
         type: 'column'
     },
     title: {
-        text: '<?php echo $_SESSION['module_name'] . " ". $year;?>'
+        text: '<?php echo $curyear;?>'
     },
     yAxis: {
         allowDecimals: false,
@@ -258,3 +259,6 @@ Highcharts.chart('container', {
 });
 
 </script>
+
+
+	
---
+++ Test Integrate/workbench_dash.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+	include('style_dc.php');
+	include('sidebar.php');
+	
+	
+	?>
+
+<div class="wrapper">
+
+
+		<div id="content">		
+		<!-- !PAGE CONTENT! -->	
+
+
+
+<html>
+<form name="find" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+
+Module Code: <input type="text" name="code"> 
+
+
+<input type="submit" name="search" value="Search">
+<input type="submit" name="select" value="Select ALL">
+
+</form>
+<br>
+</html>        
+<?php
+
+
+
+if(isset($_POST["search"])){
+	$code=$_POST['code'];
+	
+  $x=1;
+  $sql=("SELECT goal.*, module.* FROM goal JOIN module ON goal.module_id=module.module_id WHERE module.module_id='$code' GROUP BY module.module_id");
+  	
+	$result = mysql_query($sql) or die(mysql_error());
+
+
+ if(mysql_num_rows($result)>0){
+  
+	  ?>
+	  
+	  <div class="table-responsive">  
+								   <table class="table table-bordered"> 
+
+										<tr> 
+											<th>CODE</th>  
+											<th>SESSION</th>  
+											<th>NAME</th>
+										</tr>
+	  
+	  
+<?php		
+		while($row=mysql_fetch_array($result))
+		{
+			$_GET['module_id']=$row['module_id'];
+			$_GET['session_name']=$row['session_name'];
+			$name=$row['module_name'];
+			$moduleid=$_GET['module_id'];
+			$sesi=$_GET['session_name'];
+			
+			
+			
+			
+?>	
+<tr>
+                         <form action="dash_view.php" method="post">
+                            <td><?php echo $moduleid;?></td>
+								   <input type="hidden" name="moduleid" value="<?php echo $moduleid;?>"/>   
+							<td><?php echo $sesi;?></td>
+								   <input type="hidden" name="sesi" value="<?php echo $sesi;?>"/>
+							<td><?php echo $name;?></td>
+							<td><button type="submit" class="btn btn-primary" name="save">Go</button></td>
+							
+							</form>
+
+</tr>								   
+												<?php
+		}
+	$x++;											
+
+											?>
+									</table>								
+
+</div>
+
+<?php		
+ }	 
+  else
+ 	{
+		//print error message
+		echo 'No code found';
+	}
+	// once processing is complete
+	// free result set
+	
+
+}
+
+// select all
+if(isset($_POST["select"])){
+	
+	
+  $x=1;
+  $sql=("SELECT goal.*, module.* FROM goal JOIN module ON goal.module_id=module.module_id GROUP BY module.module_id");
+  	
+	$result = mysql_query($sql) or die(mysql_error());
+
+
+ if(mysql_num_rows($result)>0){
+  
+	  ?>
+	  
+	  <div class="table-responsive">  
+								   <table class="table table-bordered"> 
+
+										<tr> 
+											<th>CODE</th>  
+											<th>SESSION</th>  
+											<th>NAME</th>
+										</tr>
+	  
+	  
+<?php		
+		while($row=mysql_fetch_array($result))
+		{
+			$_GET['module_id']=$row['module_id'];
+			$_GET['session_name']=$row['session_name'];
+			$name=$row['module_name'];
+			$moduleid=$_GET['module_id'];
+			$sesi=$_GET['session_name'];
+			
+			
+			
+			
+?>	
+<tr>
+                         <form action="dash_view.php" method="post">
+                            <td><?php echo $moduleid;?></td>
+								   <input type="hidden" name="moduleid" value="<?php echo $moduleid;?>"/>   
+							<td><?php echo $sesi;?></td>
+								   <input type="hidden" name="sesi" value="<?php echo $sesi;?>"/>
+							<td><?php echo $name;?></td>
+							<td><button type="submit" name="save" class="btn btn-primary">Go</button></td>
+							<!--<td><button type="submit" name="Go" class="btn btn-primary"><a href="work_view.php?moduleid=$moduleid&sesi=$sesi">Go</a></button></td>-->
+
+							</form>
+
+</tr>								   
+												<?php
+											}
+	$x++;											
+
+											?>
+									</table>								
+
+</div>
+
+<?php		
+ }	 
+  else
+ 	{
+		//print error message
+		echo 'No code found';
+	}
+	// once processing is complete
+	// free result set
+	
+
+}
+
+
+
+	
+
+
+
+
+?>
+
+
+    </div>
+ 
+  
+</html>
+
+
